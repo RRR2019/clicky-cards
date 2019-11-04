822493274461d439d3d63c482596c13d49e3f369
Rodrigo Robles <rrobles.rodrigo@gmail.com>
2019-11-03 19:22:58 -0500

    creating images from json file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Cards from "./components/Cards";
+import characters from "./characters.json";
+
 
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
+      characters,
       score: 0,
       topScore: 0,
       clicky: true
@@ -19,7 +22,13 @@ class App extends Component {
     return (<div>
             <Navbar score={this.state.score} topScore={this.state.topScore} />
             <Header />
-            <Cards />
+            <main>
+            <div className="container col-9">
+            {this.state.characters.map(character =>(
+            <Cards src={character.image} name={character.name}/>
+            ))}
+            </div>
+            </main>
             </div>
     );
   }
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,27 +1,11 @@
 import React from "react";
+
 import "./style.css";
 
 
-function Cards() {
+function Cards(props) {
   return (
-    <main>
-      <div className="container col-9">
-      <div role="img" aria-label="click item" className="click-item" id="michael"></div>
-      <div role="img" aria-label="click item" className="click-item" id="jim"></div>
-      <div role="img" aria-label="click item" className="click-item" id="pam"></div>
-      <div role="img" aria-label="click item" className="click-item" id="dwight"></div>
-      <div role="img" aria-label="click item" className="click-item" id="kevin"></div>
-      <div role="img" aria-label="click item" className="click-item" id="phyllis"></div>
-      <div role="img" aria-label="click item" className="click-item" id="prisMike"></div>
-      <div role="img" aria-label="click item" className="click-item" id="angela"></div>
-      <div role="img" aria-label="click item" className="click-item" id="creed"></div>
-      <div role="img" aria-label="click item" className="click-item" id="tobby"></div>
-      <div role="img" aria-label="click item" className="click-item" id="erin"></div>
-      <div role="img" aria-label="click item" className="click-item" id="ryan"></div>
-
-
-      </div>
-    </main>
+      <div role="img" aria-label="click item" className="click-item" id={props.name}></div>
   )
 }
 
