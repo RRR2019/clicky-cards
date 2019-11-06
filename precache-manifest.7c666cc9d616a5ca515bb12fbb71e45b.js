8da561e9aa2e2a161fbf508a0e396f33d1bef537
Rodrigo Robles <rrobles.rodrigo@gmail.com>
2019-11-05 21:34:31 -0500

    Updates

---
+++ precache-manifest.7c666cc9d616a5ca515bb12fbb71e45b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9400ced7a54eeb10209d6ca52fbdffaf",
+    "url": "/clicky-cards/index.html"
+  },
+  {
+    "revision": "160bf2f73e44d68450e8",
+    "url": "/clicky-cards/static/css/main.bc72587d.chunk.css"
+  },
+  {
+    "revision": "9da0aa903b1aec49f2e2",
+    "url": "/clicky-cards/static/js/2.33fce524.chunk.js"
+  },
+  {
+    "revision": "160bf2f73e44d68450e8",
+    "url": "/clicky-cards/static/js/main.af285f76.chunk.js"
+  },
+  {
+    "revision": "1745b1412148a7a32d08",
+    "url": "/clicky-cards/static/js/runtime-main.6400d51c.js"
+  }
+]);
